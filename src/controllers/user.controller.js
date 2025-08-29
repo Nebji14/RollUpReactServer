cfd60e0d3daee01b5f0987d2b9a6cbf166869bc8
Nebji14 <bendav192837@gmail.com>
2025-08-29 15:35:49 +0200

    maj msg inscription+login

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -26,4 +26,33 @@ export const register = async (req, res) => {
   }
 };
 
-export const login = async (req, res) => {};
+export const login = async (req, res) => {
+  const { pseudo, password } = req.body;
+  console.log(req.body);
+
+  let user;
+
+  const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+  if (emailRegex.test(pseudo)) {
+    user = await User.findOne({ email: pseudo });
+    console.log(user);
+  } else {
+    user = await User.findOne({ pseudo: pseudo });
+    console.log(user);
+  }
+
+  if (!user) {
+    return res
+      .status(400)
+      .json({ message: "Email ou nom d'utilisateur incorrect" });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    return res.status(400).json({ message: "Mot de passe incorrect" });
+  }
+
+  // Si tout est bon
+  res.status(200).json({ user, message: "Connexion réussie" });
+};
